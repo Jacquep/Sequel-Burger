52da63943231c7242b196fb84e9e0d6e54583c49
Jacqueline Palmer <jackiemariep@yahoo.com>
2017-05-05 22:37:49 -0700

    add sequelize files

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
 //require node dependencies
 var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
 var app = express();
-
-var PORT = process.env.PORT || 3000;
-
-//Serve static content for the app from the "public" directory in the application directory.
+var path = require('path');
+app.set('port',process.env.PORT || 3000);
 app.use(express.static(process.cwd() + '/public'));
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-// Override with POST having ?_method=DELETE
+var methodOverride = require('method-override');
 app.use(methodOverride("_method"));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require('express-handlebars');
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// var routes = require('./controllers/burgers_controller.js');
+// app.use('/', routes);
 
-var routes = require('./controllers/burgers_controller.js');
-app.use('/', routes);
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs. 
+// ================================================================================
+
+require('./routing/api-routes.js')(app); 
+require('./routing/html-routes.js')(app);
 
 
-app.listen(PORT, function(){
-	console.log("Listening on " + PORT);
+app.listen(app.get('port'), function(){
+	console.log('Listening on ' + app.get('port'));
 });
